de5c1e29d2d823d9d9a9eda289da04b297650d52
Aliya Abdullah <aliya_abd2000@yahoo.in>
2022-03-21 22:01:06 +0530

    added database for starred items

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,16 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+                Search bar here
+
+                <h4>SEARCH PRODUCT: </h4>
+                <form action="searchpage" method="post">
+                
+                    <label > Name <label>
+                    <input type="text" name="item_name"><br><br>
+                    
+                    <button type="submit">Search</button>
+
             </div>
         </div>
     </div>
