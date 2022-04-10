2284e3230166d289bd97b5141d20283878f13834
Anushka2401 <56417265+Anushka2401@users.noreply.github.com>
2022-04-10 21:37:44 +0530

    controller final update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,46 +15,51 @@ class UserController extends Controller
         //$products=Items::where('name', $items)->get();
         // $products will have list of returned items by parser
         //curl to the products search api here
-            $ch = curl_init();
-        $link = ['link' => "https://www.amazon.in/dp/B09G9HD6PD/ref=redir_mobile_desktop?_encoding=UTF8&aaxitk=ef5999a9624cbf0717060ca6f1b989f2&hsa_cr_id=1681504330102&pd_rd_plhdr=t&pd_rd_r=6989883d-8966-47af-84a7-cbccd2ddfabf&pd_rd_w=loEXi&pd_rd_wg=5dWA9&ref_=sbx_be_s_sparkle_mcd_asin_0_title&th=1"];
-        curl_setopt($ch, CURLOPT_URL,"http://127.0.0.1:5000/");
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $link);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,"http://127.0.0.1:5000/".$items);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 
-
-        // Receive server response ...
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
         $server_output = curl_exec($ch);
 
-        curl_close ($ch);
-
+        curl_close($ch);
         echo $server_output;
+        
+        $output = json_decode(json_decode($server_output, true),true);
+
+        
+
+
         //clearing db for new search results
         Product::truncate();
 
         //new object of the products model
-        $p_db = new Product;
-
-         //taking all info from the json file the api returned
-        //enter correct json info here
-        $p_db->name = $server_output['Product Name'];
-        $p_db->link = $server_output['URL'];
-        $p_db->rating = $server_output['Ratings'];
-        $p_db->review = $server_output['Reviews'];
-        $p_db->price = $server_output['Price'];
-        $p_db->image = $server_output['Image'];
-        $user=auth()->user();
-        $uid = $user -> id;
-        $p_db->user_id = $uid;
-
-        //save to db
-        $p_db->save();
+        foreach($output as $key => $value){
+            $p_db = new Product;
+            //taking all info from the json file the api returned
+            //enter correct json info here
+            if($value){
+                $p_db->name = $value['Product Name'];
+                $p_db->link = $value['URL'];
+                $p_db->rating = $value['Ratings'];
+                $p_db->review = $value['Reviews'];
+                $p_db->price = $value['Price'];
+                //$p_db->image = $value['Image'];
+                
+                $user=auth()->user();
+                $uid = $user -> id;
+                $p_db->user_id = $uid;
+    
+            //save to db
+                $p_db->save();
+            }
+        }
+       
+        
 
         //returns json of products to the searchpage
-        return view('searchpage', ['products'=>$products]);
+        return view('searchpage', ['products'=>$server_output]);
     }
-
     public function productinfo($id){
  
         $product= Product::find($id);
