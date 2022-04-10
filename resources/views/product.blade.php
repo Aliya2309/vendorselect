13fbddbd4096535df91b7a5707fd3a7772733f6c
Anushka2401 <56417265+Anushka2401@users.noreply.github.com>
2022-04-10 21:53:52 +0530

    commit on product page

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -1,7 +1,24 @@
 <a href="/"> Go Back </a><br><br>
 
 <?php
+$strlink = strval($product['link']);
+echo $strlink;
+$ch = curl_init();
+$link = array('link' => $strlink);
+$abc = json_encode($link, JSON_FORCE_OBJECT);
+curl_setopt($ch, CURLOPT_URL,"http://127.0.0.1:5000/");
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $abc);
 
+
+// Receive server response ...
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$server_output = curl_exec($ch);
+
+curl_close ($ch);
+
+echo $server_output;
 $product_name = $product['name'];
 
 $url = "http://127.0.0.1:5000/";
