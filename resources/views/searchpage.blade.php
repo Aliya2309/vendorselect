b077b904623958e314c610fe18e60ff27ef5a6c4
Anushka2401 <56417265+Anushka2401@users.noreply.github.com>
2022-04-10 21:44:21 +0530

    new commit
    the new code is very brief

--- resources/views/searchpage.blade.php
+++ resources/views/searchpage.blade.php
@@ -1,6 +1,22 @@
 <a href="/"> Go Back </a><br><br>
 <h4>Your Search Produced the following results:  </h4><br><br>
 
+//i didn't know if i should delete the code originally there so i added the new code here
+
+<a href="/"> Go Back </a><br><br>
+<h4>Your Search Produced the following results:  </h4><br><br>
+<?php
+$output = json_decode(json_decode($products, true),true);
+foreach($output as $p){
+    echo "NAME:   ".$p['Product Name'].'<br><br>';
+     echo "LINK:".$p['URL'].'<br><br>';
+     echo "RATINGS:  ".$p['Ratings'].'<br><br>';
+    echo "REVIEWS:  ".$p['Reviews'].'<br><br>';
+    echo "PRICE:  ".$p['Price'].'<br><br>';
+    
+ }
+ //new code ends
+
 @foreach($products as $i)
 <div> 
     <br>
@@ -17,9 +33,9 @@
     <div> 
     <br>
     <a href="product/{{$i['Price']}}">{{$i->price}}</a></div>
-    <div> 
-    <br>
-    <a href="product/{{$i['Image']}}">{{$i->image}}</a></div>
+    //<div> 
+    //<br>
+    //<a href="product/{{$i['Image']}}">{{$i->image}}</a></div>
 <!--<div> {{$i->description}}</div>
 <div> Rupees. {{$i->price}}</div>
 <div> Category: {{$i->category}}</div> -->
